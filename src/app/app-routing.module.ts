a09240c9b86ca0dca6356d71fb9903de714f78e4
IsaacGuerra <isguerra@sudamericano.edu.ec>
2024-08-19 16:50:48 -0500

    Update Components
    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,8 @@ import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'information', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
-  { path: 'home', loadComponent: () => import('./pages/information/information.component').then(m => m.InformationComponent) },
+  { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
+  { path: 'information', loadComponent: () => import('./pages/information/information.component').then(m => m.InformationComponent) },
   { path: 'themes', loadComponent: () => import('./pages/themes/themes.component').then(m => m.ThemesComponent) },
   { path: 'speakers', loadComponent: () => import('./pages/speakers/speakers.component').then(m => m.SpeakersComponent) },
   { path: 'register', loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent) }
