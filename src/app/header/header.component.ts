2004a887f1991216fb18940d55f76b7fb209dc2d
IsaacGuerra <isguerra@sudamericano.edu.ec>
2024-08-20 12:41:15 -0500

    Navbar Update
    Changes for navigation bar refresh and responsive styling

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   menuValue: boolean = false;
