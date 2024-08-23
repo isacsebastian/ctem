5d9bf2384b4340c7cb907ea4fadfb3b9b89ad837
IsaacGuerra <isguerra@sudamericano.edu.ec>
2024-08-22 22:08:14 -0500

    Revert "Merge branch 'master' of https://github.com/isacsebastian/ctem"
    This reverts commit 9c0acd34722d6035dc83e01cc1b39fd463a09a48, reversing
    changes made to 9a871c45632f52e044caa747a4d3148edb836b3b.

--- src/app/pages/information/information.component.ts
+++ src/app/pages/information/information.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../components/button/button.component';
+import { Button2Component } from '../../components/button2/button2.component';
 import { FooterComponent } from '../../components/footer/footer.component';
-import { CarouselimgComponent } from '../../components/carouselimg/carouselimg.component';
-
 
 @Component({
   selector: 'app-information',
   standalone: true,
-  imports: [ButtonComponent, FooterComponent, CarouselimgComponent],
+  imports: [ButtonComponent, Button2Component, FooterComponent],
   templateUrl: './information.component.html',
   styleUrl: './information.component.css'
 })
--- src/app/services/strapi.service.ts
+++ src/app/services/strapi.service.ts
@@ -1,29 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Register } from '../models/register';
+import { Testimonials } from '../models/testimonials';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StrapiService {
-  private url:string = 'http://localhost:1337/api/registers'
+  private url:string = 'http://localhost:1337/api'
   constructor(private http: HttpClient) { }
 
-  getRegisters():Observable<Register[]>{
-    return this.http.get<Register[]>(`${this.url}`)
-  }
-
-  addRegisters(register: any): Observable<any[]> {
-    return this.http.post<any[]>(`${this.url}`, { data: register });
-  }
-  
-  
-  deleteRegisters(id:number):Observable<Register[]>{
-    return this.http.delete<Register[]>(`${this.url}/${id}`)
-  }
-
-  updateRegisters(Register: Register):Observable<Register[]>{
-    return this.http.put<Register[]>(`${this.url}/`,Register)
+  getTestimonials():Observable<Testimonials[]>{
+    return this.http.get<Testimonials[]>(`${this.url}/testimonials`)
   }
 }
