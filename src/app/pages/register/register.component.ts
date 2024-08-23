df152cef035c8b4b7047421d7f71f57cf6accefa
IsaacGuerra <isguerra@sudamericano.edu.ec>
2024-08-22 22:08:04 -0500

    Reapply "Merge branch 'master' of https://github.com/isacsebastian/ctem"
    This reverts commit 1518aadc4b09626d80891b6896c790c332c9fd6e.

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators, NonNullableFormBuilder } from '@angular/forms';
+import { StrapiService } from '../../services/strapi.service';
+import { Register } from '../../models/register';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css'] // Corrección en la propiedad styleUrl
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  user: FormGroup<{
+    fullname: FormControl<string>;
+    email: FormControl<string>;
+    phone: FormControl<string>;
+  }>;
 
+  successMessage: string = '';
+
+  constructor(
+    private fb: NonNullableFormBuilder,
+    private strapiService: StrapiService // Cambié el nombre del parámetro para mayor claridad
+  ) {
+    const { required, pattern, minLength, maxLength } = Validators;
+
+    this.user = this.fb.group({
+      fullname: [
+        '', 
+        [
+          required, 
+          minLength(8), 
+          pattern('^[a-zA-Z\\s]*$') // Valida que solo contenga letras y espacios
+        ]
+      ],
+      email: [
+        '', 
+        [
+          required, 
+          pattern('^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$')
+        ]
+      ],
+      phone: [
+        '', 
+        [
+          required, 
+          pattern('^[0-9]*$'), 
+          minLength(10), 
+          maxLength(10)
+        ]
+      ], 
+    });
+  }
+
+  registers!: Register;
+  title: string = 'Damian';
+
+  ngOnInit(): void {
+    this.loadRegisters();
+  }
+
+  loadRegisters(): void {
+    this.strapiService.getRegisters().subscribe((data: any) => {
+      console.log(data.data[0].attributes);
+    });
+  }
+
+  validateForm(): void {
+    if (this.user.valid) {
+      this.strapiService.addRegisters(this.user.value).subscribe((response: any) => {
+        console.log(response);
+        this.successMessage = 'Registrado exitosamente. Revisar su WhatsApp y correo para más información.';
+        this.user.reset(); // Limpiar el formulario después de enviar
+      }, error => {
+        console.error(error);
+      });
+    }
+  }
 }
--- src/app/services/strapi.service.ts
+++ src/app/services/strapi.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Testimonials } from '../models/testimonials';
+import { Register } from '../models/register';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StrapiService {
-  private url:string = 'http://localhost:1337/api'
+  private url:string = 'http://localhost:1337/api/registers'
   constructor(private http: HttpClient) { }
 
-  getTestimonials():Observable<Testimonials[]>{
-    return this.http.get<Testimonials[]>(`${this.url}/testimonials`)
+  getRegisters():Observable<Register[]>{
+    return this.http.get<Register[]>(`${this.url}`)
+  }
+
+  addRegisters(register: any): Observable<any[]> {
+    return this.http.post<any[]>(`${this.url}`, { data: register });
+  }
+  
+  
+  deleteRegisters(id:number):Observable<Register[]>{
+    return this.http.delete<Register[]>(`${this.url}/${id}`)
+  }
+
+  updateRegisters(Register: Register):Observable<Register[]>{
+    return this.http.put<Register[]>(`${this.url}/`,Register)
   }
 }
