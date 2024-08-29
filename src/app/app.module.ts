fcf766eb39b87ceb2f7f1044ceea0e9cda79eabf
IsaacGuerra <isguerra@sudamericano.edu.ec>
2024-08-29 08:27:05 -0500

    update carousel import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component'; 
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -14,7 +16,9 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,11 +4,13 @@ import { CarouselimgComponent } from '../../components/carouselimg/carouselimg.c
 import { ButtonComponent } from '../../components/button/button.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import {Carousel03Component} from '../../components/carousel/carousel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonComponent, CardsComponent, CarouselimgComponent, FooterComponent],
+  imports: [ButtonComponent, CardsComponent, CarouselimgComponent, FooterComponent, RouterLink, Carousel03Component],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
